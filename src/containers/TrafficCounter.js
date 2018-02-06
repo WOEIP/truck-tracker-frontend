a05657c1c53c75648c63335b4e10d89e7e776e99
Attila Kovacs <akovacs101386@gmail.com>
2018-02-05 19:46:13 -0800

    React counter

--- src/containers/TrafficCounter.js
+++ src/containers/TrafficCounter.js
@@ -8,15 +8,38 @@ import Button from '../components/button.js';
 const R = React.createElement;
 
 class TrafficCounter extends React.Component {
-  buttonHandler(){
-    console.log('yay');
+  constructor (props){
+    super(props);
+    this.state= {
+      count: 1
+    };
+  };
+
+  increment(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement(){
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  send(){
+    console.log('State: ' + this.state.count.toString() + ', sent');
   };
 
   render (){
     return (
       R('div', null,
-       R('p', {'className': 'colored'}, 'hey'),
-        Button({'handleClick': this.increment, 'label': 'Plus'})));
+        R('p', {'className': 'colored'},
+          'State: ' + this.state.count.toString()),
+        Button({'handleClick': this.increment.bind(this), 'label': 'Plus'}),
+        Button({'handleClick': this.decrement.bind(this), 'label': 'Minus'}),
+        Button({'handleClick': this.send.bind(this), 'label': 'Send'}),
+       ));
   }
 }
 
