a57e9255ae97c9e1154a07eb93854c703d866203
Attila Kovacs <akovacs101386@gmail.com>
2018-03-20 18:56:55 -0500

    Add hash based routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,16 +6,41 @@ import Contact from './Contact';
 import Footer from './Footer';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.getActiveContent = this.getActiveContent.bind(this); //do I need this
+  }
+
+  componentDidMount() {
+    var self = this;
+    window.addEventListener('hashchange', (e) => {self.forceUpdate();});
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('hashchange', (e) => {self.forceUpdate();});
+  }
+
+  getActiveContent(){
+    switch(window.location.hash) {
+    case '#report':
+      return Report;
+    case '#mission':
+      return Mission;
+    case '#contact':
+      return Contact;
+    case '':
+      return Header;
+    default:
+      return Header;
+    }
+  };
+
   render() {
+    const ActiveContent = this.getActiveContent();
     return (
       <div>
-          <Header/>
-          <div id="main">
-            <Report/>
-            <Mission/>
-            <Contact/>
-          </div>
-          <Footer/>
+        <ActiveContent/>
       </div>
     );
   }
