aa943ca567e42c807b75e5f86d7c4cd8d91ba5fb
Attila Kovacs <akovacs101386@gmail.com>
2018-03-30 17:27:04 -0700

    Contact page CSS

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,55 +1,51 @@
 import React, { Component } from 'react';
+import Menu from './../components/Menu.js';
+import '../styles/common.scss';
+import '../styles/contact.scss';
+import '../styles/pure-release-1.0.0/pure-min.css';
+import '../styles/pure-release-1.0.0/grids-responsive.css';
 
 class Contact extends Component {
   render() {
     return (
-      <article id="contact">
-        <h2 className="major">Contact</h2>
-        <form method="post" action="#">
-          <div className="field half first">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
-          </div>
-          <div className="field half">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
-          </div>
-          <div className="field">
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="4"></textarea>
-          </div>
-          <ul className="actions">
+      <div className="bground">
+        <article id="contact">
+          <Menu current="contact"/>
+          <h1 className="title">Send a message</h1>
+          <p>Use the form below, and we'll get back to you!</p>
+          <form className="pure-g" method="post" action="#">
+            <div className="form-element pure-u-1">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="form-element pure-u-1">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="form-element pure-u-1">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li>
+                <input type="submit" value="Send"/>
+              </li>
+            </ul>
+          </form>
+          <ul className="icons">
             <li>
-              <input type="submit" value="Send Message" className="special" />
+              <a href="" className="icon fa-twitter">
+                <span className="label">Twitter</span>
+              </a>
             </li>
             <li>
-              <input type="reset" value="Reset" />
+              <a href="" className="icon fa-facebook">
+                <span className="label">Facebook</span>
+              </a>
             </li>
           </ul>
-        </form>
-        <ul className="icons">
-          <li>
-            <a href="" className="icon fa-twitter">
-              <span className="label">Twitter</span>
-            </a>
-          </li>
-          <li>
-            <a href="" className="icon fa-facebook">
-              <span className="label">Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a href="" className="icon fa-instagram">
-              <span className="label">Instagram</span>
-            </a>
-          </li>
-          <li>
-            <a href="" className="icon fa-github">
-              <span className="label">GitHub</span>
-            </a>
-          </li>
-        </ul>
-      </article>
+        </article>
+      </div>
     );
   }
 }
